7044cb58d12dc0ef14a8b340096eabfe8762776b
soongjamm <kimstz0303@gmail.com>
2020-11-03 22:12:49 +0900

    3-11 댓글

--- pybo/__init__.py
+++ pybo/__init__.py
@@ -30,12 +30,19 @@ def create_app():
     from . import models
 
     # blueprint
-    from .views import main_views, question_views, answer_views, auth_views
+    from .views import (
+        main_views,
+        question_views,
+        answer_views,
+        auth_views,
+        comment_views,
+    )
 
     app.register_blueprint(main_views.bp)
     app.register_blueprint(question_views.bp)
     app.register_blueprint(answer_views.bp)
     app.register_blueprint(auth_views.bp)
+    app.register_blueprint(comment_views.bp)
 
     # 필터
     from .filter import format_datetime
