0ad238352f7ff75d850f095859c9ce5fd1617938
soongjamm <kimstz0303@gmail.com>
2020-11-03 13:38:47 +0900

    계정생성

--- pybo/__init__.py
+++ pybo/__init__.py
@@ -19,14 +19,18 @@ def create_app():
     from . import models
 
     # blueprint
-    from .views import main_views, question_views, answer_views
+    from .views import main_views, question_views, answer_views, auth_views
 
     app.register_blueprint(main_views.bp)
     app.register_blueprint(question_views.bp)
     app.register_blueprint(answer_views.bp)
+    app.register_blueprint(auth_views.bp)
 
     # 필터
     from .filter import format_datetime
-    app.jinja_env.filters['datetime'] = format_datetime # app.jinja_env.filters['datetime'] 처럼 datetime 이라는 이름의 필터로 등록해 주었다. 이제 템플릿에서 |datetime 으로 사용
+
+    app.jinja_env.filters[
+        "datetime"
+    ] = format_datetime  # app.jinja_env.filters['datetime'] 처럼 datetime 이라는 이름의 필터로 등록해 주었다. 이제 템플릿에서 |datetime 으로 사용
 
     return app
--- pybo/models.py
+++ pybo/models.py
@@ -1,5 +1,10 @@
 from pybo import db
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
 
 class Question(db.Model):
     id = db.Column(db.Integer, primary_key=True)
