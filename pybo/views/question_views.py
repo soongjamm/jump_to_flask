5f8378a5ba8a0e0ba6c7b8403f7da7cb6ec877ab
soongjamm <kimstz0303@gmail.com>
2020-11-04 20:31:42 +0900

    검색과 정렬

--- pybo/views/question_views.py
+++ pybo/views/question_views.py
@@ -2,10 +2,11 @@ from datetime import datetime
 
 from flask import Blueprint, render_template, request, url_for, g, flash
 from werkzeug.utils import redirect
+from sqlalchemy import func
 
 from .auth_views import login_required
 from .. import db
-from ..models import Question
+from ..models import Question, Answer, User, question_voter
 from ..forms import QuestionForm, AnswerForm
 
 bp = Blueprint("question", __name__, url_prefix="/question")
@@ -13,12 +14,66 @@ bp = Blueprint("question", __name__, url_prefix="/question")
 
 @bp.route("/list/")
 def _list():
-    page = request.args.get("page", type=int, default=1)  # 페이지값을 가져옴
-    question_list = Question.query.order_by(Question.create_date.desc())
+    # 입력 파라미터
+    page = request.args.get("page", type=int, default=1)
+    kw = request.args.get("kw", type=str, default="")
+    so = request.args.get("so", type=str, default="recent")
+
+    # 정렬
+    if so == "recommend":
+        sub_query = (
+            db.session.query(
+                question_voter.c.question_id, func.count("*").label("num_voter")
+            )
+            .group_by(question_voter.c.question_id)
+            .subquery()
+        )
+        question_list = Question.query.outerjoin(
+            sub_query, Question.id == sub_query.c.question_id
+        ).order_by(sub_query.c.num_voter.desc(), Question.create_date.desc())
+    elif so == "popular":
+        sub_query = (
+            db.session.query(Answer.question_id, func.count("*").label("num_answer"))
+            .group_by(Answer.question_id)
+            .subquery()
+        )
+        question_list = Question.query.outerjoin(
+            sub_query, Question.id == sub_query.c.question_id
+        ).order_by(sub_query.c.num_answer.desc(), Question.create_date.desc())
+    else:  # recent
+        question_list = Question.query.order_by(Question.create_date.desc())
+    # 조회
+    if kw:
+        search = "%%{}%%".format(kw)
+        sub_query = (
+            db.session.query(Answer.question_id, Answer.content, User.username)
+            .join(User, Answer.user_id == User.id)
+            .subquery()
+        )
+        question_list = (
+            question_list.join(User)
+            .outerjoin(sub_query, sub_query.c.question_id == Question.id)
+            .filter(
+                Question.subject.ilike(search)
+                | Question.content.ilike(search)
+                | User.username.ilike(search)
+                | sub_query.c.content.ilike(search)
+                | sub_query.c.username.ilike(search)
+            )
+            .distinct()
+        )
+
+    # 페이징
     question_list = question_list.paginate(
         page, per_page=10
     )  # Pagination 객체로 변경되었다. 이제 템플릿에서 함수 사용가능. (.items)
-    return render_template("question/question_list.html", question_list=question_list)
+    return render_template(
+        "question/question_list.html",
+        question_list=question_list,
+        page=page,
+        kw=kw,
+        so=so,
+    )
 
 
 @bp.route("/detail/<int:question_id>/")
